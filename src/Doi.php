c8ab9e2243aca7554f3a82cfdae792dfa4da1060
Paul Mucur <paul@altmetric.com>
2017-04-07 16:34:21 +0100

    Support valid DOIs ending in punctuation
    During some internal data clean-up work at @altmetric, we discovered
    DOIs that end with closing parentheses. These were being dropped by our
    extraction regular expression as we enforce a word boundary (which
    splits when a word character is followed by a non-word character such as
    a closing parenthesis).
    
    By removing the strict word boundary, we can now pick up these closing
    parentheses and filter out any unwanted trailing punctuation in a second
    pass over the matches.
    
    We do this by explicitly dropping the last character if it is
    punctuation but _not_ if the DOI ends in a single, balanced
    parenthetical group of characters or if it is an early Wiley DOI (which
    end in a certain sequence of punctuation).
    
    This means that we can successfully extract valid DOIs such as
    10.1002/0471221929.ch26(v) but not invalid ones such as
    10.1007/978-3-319-12580-0).

--- src/Doi.php
+++ src/Doi.php
@@ -3,10 +3,48 @@ namespace Altmetric\Identifiers;
 
 class Doi
 {
+    const PATTERN = <<<'EOT'
+{
+    10 # Directory indicator (always 10)
+    \.
+    (?:
+        # ISBN-A
+        97[89]\. # ISBN (GS1) Bookland prefix
+        \d{2,8}  # ISBN registration group element and publisher prefix
+        /        # Prefix/suffix divider
+        \d{1,7}  # ISBN title enumerator and check digit
+        |
+        # DOI
+        \d{4,9} # Registrant code
+        /       # Prefix/suffix divider
+        \S+     # DOI suffix
+    )
+}x
+EOT;
+    const VALID_TRAILING_PUNCTUATION = <<<'EOT'
+/
+    (?:
+        \(.+\) # Balanced parentheses
+        |
+        2-\#   # Early Wiley DOI suffix
+    )
+    $
+/x
+EOT;
+
     public static function extract($str)
     {
-        preg_match_all('#\b10\.(?:97[89]\.\d{2,8}/\d{1,7}|\d{4,9}/\S+)\b#', $str, $matches);
+        preg_match_all(self::PATTERN, strtolower($str), $matches);
 
-        return array_map('strtolower', $matches[0]);
+        return array_map([__CLASS__, 'stripTrailingPunctuation'], $matches[0]);
+    }
+
+    private static function stripTrailingPunctuation($doi)
+    {
+        if (!preg_match('/[[:punct:]]$/', $doi) || preg_match(self::VALID_TRAILING_PUNCTUATION, $doi)) {
+            return $doi;
+        }
+
+        return preg_replace('/[[:punct:]]+$/', '', $doi);
     }
 }
