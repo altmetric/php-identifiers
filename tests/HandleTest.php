00f7b3d68c319b7b1adb5f050f3451a887f20ff7
Paul Mucur <paul@altmetric.com>
2017-04-10 14:38:57 +0100

    Improve Handle extraction from Unicode strings

--- tests/HandleTest.php
+++ tests/HandleTest.php
@@ -7,4 +7,9 @@ class HandleTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals(['10149/596901'], Handle::extract('http://hdl.handle.net/10149/596901'));
     }
+
+    public function testExtractsHandlesWithTrailingUnicodeWhitespace()
+    {
+        $this->assertEquals(['10149/596901'], Handle::extract('http://hdl.handle.net/10149/596901 '));
+    }
 }
